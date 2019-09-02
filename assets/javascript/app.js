f0a53fb8ae8d8ce874b1439b4cda1a483786eb2d
Nick Ramsay <NickRamsay1989@gmail.com>
2019-09-02 20:50:54 +1000

    Added jQuery to append gif divs

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,11 +1,11 @@
 $(document).ready(function () {
-    var allButtons = ["Fail", "Pundog"];
+    var allButtons = [];
 
     function renderButtons() {
         $(".buttons").empty();
         for (i = 0; i < allButtons.length; i++) {
             var newButtonSearch = allButtons[i];
-            var newButton = $('<button class="btn btn-dark btn-sm m-1 gifButton" data-search=' + newButtonSearch + '>' + newButtonSearch + '</button>');
+            var newButton = $('<button class="btn btn-dark btn-sm m-1 gifButton" data-topic=' + newButtonSearch + '>' + newButtonSearch + '</button>');
             $(".buttons").append(newButton);
         }
     };
@@ -16,13 +16,42 @@ $(document).ready(function () {
         $("#newButtonSearch").val("");
     })
 
-    function generateGifs() {
-        var gifSearch = $(this).attr("data-search");
+    $(document).on("click", ".gifButton", function () {
+        $(".gifs").empty();
+        var gifSearch = $(this).attr("data-topic");
         var queryURL = "https://api.giphy.com/v1/gifs/search?api_key=kVnhBzTc4hSQFqIP3Xg2CIuos2Lmj5ze&q=" + gifSearch + "&limit=10&offset=0&rating=G&lang=en";
-        console.log(queryURL);
-    }
+        console.log(gifSearch);
 
-    $(document).on("click", ".gifButton", generateGifs);
+        $.ajax({
+            url: queryURL,
+            method: "GET"
+        }).then(function (response) {
+            $(".gifs").empty();
+            var results = response.data;
+            for (var i = 0; i < results.length; i++) {
+                /*var cardDiv = $('<div class="card" style="width: 18rem;">');
+                var imageDiv = $('<img src="' + results.url + '" class="card-img-top" alt="' + results.title + '">');
+                var cardBodyDiv = $('<div class="card-body">');
+                var ratingDiv = $('<p> Rating: ' + results.rating + '</p>');
+
+                $(cardDiv).append(imageDiv);
+                $(cardBodyDiv).append(ratingDiv);
+                $(cardDiv).append(cardBodyDiv);
+                $(".gifs").append(cardDiv);
+                */
+                var gifContainerDiv = $('<div class="card" style="width: 18rem;">');
+                var imageDiv = $('<img class="card-img-top" alt=' + results.title + '>');
+                var cardBodyDiv = $('<div class="card-body">');
+                var ratingDiv = $("<p>Rating: " + results[i].rating + "</p>");
+                $(imageDiv).attr("src", results[i].images.fixed_height.url);
+                $(cardBodyDiv).append(ratingDiv);
+                $(gifContainerDiv).append(imageDiv);
+                $(gifContainerDiv).append(cardBodyDiv);
+                $(".gifs").append(gifContainerDiv);
+            }
+        })
+    }
+    );
 
     window.onload = renderButtons();
 })
