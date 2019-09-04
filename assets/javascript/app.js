d77e3af855346fb69325df2aae733f7c2a49ae30
Nick Ramsay <NickRamsay1989@gmail.com>
2019-09-04 21:04:32 +1000

    added remove and clear functions to favourites

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -16,7 +16,7 @@ $(document).ready(function () {
         $("#newButtonSearch").val("");
     })
 
-
+    uniqueGifID = 0;
 
     $(document).on("click", ".gifButton", function () {
         $(".gifs").empty();
@@ -31,16 +31,23 @@ $(document).ready(function () {
             $(".gifs").empty();
             var results = response.data;
             for (var i = 0; i < results.length; i++) {
+                uniqueGifID ++;
                 var gifContainerDiv = $('<div class="card float-left" style="width: 18rem;">');
-                $(gifContainerDiv).attr("id", gifSearch + i);
+                $(gifContainerDiv).attr("id", gifSearch + uniqueGifID);
+                
                 var imageDiv = $('<img class="card-img-top gifImage" alt=' + results.title + '>');
-                var cardBodyDiv = $('<div class="card-body">');
-                var ratingDiv = $("<span>Rating: " + results[i].rating.toUpperCase() + "</span>");
-                var favouriteBtn = $("<button class='float-right favouriteBtn'>Fav</button></p>")
-                $(favouriteBtn).attr("data-gif-ID", gifSearch + i);
                 $(imageDiv).attr("data-still-url", results[i].images.fixed_height_still.url);
                 $(imageDiv).attr("data-play-url", results[i].images.fixed_height.url);
                 $(imageDiv).attr("src", $(imageDiv).attr("data-play-url"));
+
+                var cardBodyDiv = $('<div class="card-body gifText">');
+                $(cardBodyDiv).attr("id", gifSearch + uniqueGifID + "cardBody");
+
+                var ratingDiv = $("<span>Rating: " + results[i].rating.toUpperCase() + "</span>");
+                var favouriteBtn = $("<button class='float-right favouriteBtn'>Fav</button>");
+                $(favouriteBtn).attr("id", gifSearch + uniqueGifID + "button");
+                $(favouriteBtn).attr("data-gif-ID", gifSearch + uniqueGifID);
+                
                 $(cardBodyDiv).append(ratingDiv);
                 $(cardBodyDiv).append(favouriteBtn);
                 $(gifContainerDiv).append(imageDiv);
@@ -67,10 +74,21 @@ $(document).ready(function () {
     })
 
     $(document).on("click",".favouriteBtn", function addFavourite () {
-        $("#"+ $(this).attr("data-gif-ID")).appendTo(".favourites");
+        $("#" + $(this).attr("data-gif-ID")).appendTo(".favourites");
+        $("#" + $(this).attr("id")).remove();
+        var removeBtn = $("<button class='float-right removeBtn'>Remove</button>");
+        $(removeBtn).attr("data-gif-ID", $(this).attr("data-gif-ID"));
+        $('#' + $(this).attr('data-gif-ID') + 'cardBody').append(removeBtn);
+    }) //adds gif to favourites section
+
+    $(document).on("click",".removeBtn", function removeFavourite() {
+        $('#' + $(this).attr('data-gif-ID')).remove();
+        console.log($('#' + $(this).attr('data-gif-ID')));
     })
 
-
+    $(".clearFavourites").on("click", function clearFavourites() {
+        $(".favourites").empty();
+    })
 
     window.onload = renderButtons();
 })
